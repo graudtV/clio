0730440a6a08955ee8c4c33f54754bf7217b8457
graudtV <55806873+graudtV@users.noreply.github.com>
2021-08-23 00:09:41 +0300

    explicitly require Boost

--- cmake/clioConfig.cmake
+++ cmake/clioConfig.cmake
@@ -4,3 +4,8 @@ add_library(clio STATIC IMPORTED)
 
 find_library(clio_LIBRARY_DIR clio HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
 set(clio_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../include")
+
+if ((NOT DEFINED ${Boost_FOUND}) OR (NOT ${Boost_FOUND}))
+	find_package(Boost REQUIRED)
+	message(STATUS "clio found Boost at: ${Boost_INCLUDE_DIR}")
+endif()
