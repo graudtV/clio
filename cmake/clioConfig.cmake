4b3fa84b4c2a4a7cd5ee0b49eaa5b176b22dd4f7
graudtV <55806873+graudtV@users.noreply.github.com>
2021-09-12 02:03:49 +0300

    simplify external clio look-up with cmake

--- cmake/clioConfig.cmake
+++ cmake/clioConfig.cmake
@@ -4,8 +4,12 @@ include("${CMAKE_CURRENT_LIST_DIR}/clio_deps.cmake")
 
 add_library(clio STATIC IMPORTED)
 
-find_library(clio_LIBRARY_DIR clio HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
+find_library(clio_LIBRARY_FILE clio HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
 set(clio_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../include")
+
+target_sources(clio INTERFACE ${clio_LIBRARY_FILE})
+target_include_directories(clio SYSTEM INTERFACE ${clio_INCLUDE_DIR})
+set_target_properties(clio PROPERTIES IMPORTED_LOCATION ${clio_LIBRARY_FILE})
 
 if ((NOT DEFINED ${Boost_FOUND}) OR (NOT ${Boost_FOUND}))
 	find_package(Boost REQUIRED)
